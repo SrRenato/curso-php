d4299a05e5b532b12fe5531586e6c6cf8847311d
Renatin <renatoa087@gmail.com>
2023-09-13 16:00:29 -0300

    first commit

---
+++ variaveis/basico.php
@@ -0,0 +1,32 @@
+<div class="titulo">variaveis</div>
+
+<?php
+
+$numeroA = 13;
+echo $numeroA, '<br>';
+var_dump($numeroA);
+
+echo '<br>';
+$a = 3;
+$b = 16;
+$somaDosNumeros = $a + $b;
+
+echo $somaDosNumeros;
+
+echo '<br>';
+echo isset($somaDosNumeros);
+
+unset($somaDosNumeros);
+echo '<br>';
+var_dump($somaDosNumeros);
+
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = "Agora sou uma string!";
+echo '<br>' . $variavel;
+
+
+// nome de variaveis
+$var = 'valida';
+$var2 = 'valida';
